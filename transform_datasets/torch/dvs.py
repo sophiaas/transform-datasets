564c587ce0cf5e0c4de33d1e41d6fbf78a3e5d57
ssanborn <sophia.sanborn@intel.com>
2021-01-25 15:49:25 -0800

    restructures

--- transform_datasets/torch/dvs.py
+++ transform_datasets/torch/dvs.py
@@ -2,6 +2,25 @@ import torch
 from torch.utils.data import Dataset
 import numpy as np
 from transform_datasets.numpy.dvs import collapse_data, get_hot_pixels, erase_hot_pixels
+from transform_datasets.numpy.dvs import dvs_to_vid as dvs_to_vid_numpy
+
+
+def dvs_to_vid(x, y=None, t=None, p=None, frame_rate=120, img_size=(260, 346)):
+    
+    if y is None:
+        y = x[:, 1]
+        t = x[:, 2]
+        p = x[:, 3]
+        x = x[:, 0]
+    
+    x = x.numpy()
+    y = y.numpy()
+    t = t.numpy()
+    p = p.numpy()
+    
+    projections = dvs_to_vid_numpy(x, y, t, p, frame_rate, img_size)
+    
+    return torch.tensor(projections)
 
 
 class DVSMotion20(Dataset):
@@ -14,8 +33,8 @@ class DVSMotion20(Dataset):
                  n_stds=5
                  ):
 
-        filename = '/home/ssanborn/data/DVSMOTION20/camera-motion-data/{}_sequence/events.npy'.format(sequence)
-        of_filename = '/home/ssanborn/data/DVSMOTION20/camera-motion-data/{}_sequence/optic_flow.npy'.format(sequence)
+        filename = '/home/ssanborn/data/DVSMOTION20/camera-motion-data/{}_sequence/events_clean.npy'.format(sequence)
+        of_filename = '/home/ssanborn/data/DVSMOTION20/camera-motion-data/{}_sequence/optic_flow_clean.npy'.format(sequence)
         
         self.img_size = (260, 346)
         
@@ -40,14 +59,23 @@ class DVSMotion20(Dataset):
         self.p = torch.tensor(p)
         self.optic_flow = torch.tensor(optic_flow)
         
+    def project(self, frame_rate):
+        projected_vid = dvs_to_vid(x=self.x, 
+                                   y=self.y, 
+                                   t=self.t, 
+                                   p=self.p, 
+                                   frame_rate=frame_rate, 
+                                   img_size=self.img_size)
+        return projected_vid
+        
     @property
     def pos(self):
-        return np.vstack([self.x, self.y, self.t]).T
+        return torch.vstack([self.x, self.y, self.t]).T
     
     @property
     def data(self):
         return self.x, self.y, self.t, self.p
-        
+
     def __getitem__(self, idx, flow=False):
         x = self.x[idx]
         y = self.y[idx]
@@ -61,3 +89,4 @@ class DVSMotion20(Dataset):
     
     def __len__(self):
         return len(self.x)
+    
